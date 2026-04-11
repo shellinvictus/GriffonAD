428bbe2d0e985fc833ab10d1ed521d3bdc6a590a
shellinvictus <shellinvictus@localhost>
2026-04-11 18:07:03 +0200

    0.6.18

--- griffonad/griffon.py
+++ griffonad/griffon.py
@@ -166,7 +166,7 @@ def main():
     args = parser.parse_args()
 
     if args.version:
-        print('GriffonAD 0.6.17')
+        print('GriffonAD 0.6.18')
         print('https://github.com/shellinvictus/GriffonAD')
         exit(0)
 
